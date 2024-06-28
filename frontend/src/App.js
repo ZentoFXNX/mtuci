129e9665e0f8209940f128cf3a4752167d43e0d2
ZentoFX <nikitosikgrifer@mail.ru>
2024-06-28 12:46:25 +0300

    Initial commit with frontend and backend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="Main">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
